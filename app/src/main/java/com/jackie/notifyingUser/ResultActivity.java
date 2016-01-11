f7fb5060f53851f310a30aac7055d2486cd97765
jackie <zhuweiping@madxstudio.cn>
2016-01-11 21:50:45 +0800

    Add Apache License 2.0 description

--- app/src/main/java/com/jackie/notifyingUser/ResultActivity.java
+++ app/src/main/java/com/jackie/notifyingUser/ResultActivity.java
@@ -19,11 +19,11 @@ package com.jackie.notifyingUser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
     }
 }
